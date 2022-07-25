2c0c17c90f860dd6851b81b33daf6e6e10dd69d1
unknown <Lingzhi.Wang@fuchuang.com>
2022-07-25 19:18:54 +0800

    单选开关、复选框、输入框及表单icon学习

--- lib/components_study/basic_components.dart
+++ lib/components_study/basic_components.dart
@@ -8,6 +8,12 @@ class BasicComponents extends StatefulWidget {
 }
 
 class _BasicComponentsState extends State<BasicComponents> {
+  bool _switchSelected = true; // 维护单选开关状态
+  bool _checkboxSelected = true; // 维护复选框状态
+
+  final TextEditingController _unameController = TextEditingController();
+  final GlobalKey _formKey = GlobalKey<FormState>();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -64,7 +70,76 @@ class _BasicComponentsState extends State<BasicComponents> {
               Image.network(
                 "https://uploadfile.bizhizu.cn/up/ff/77/64/ff77643d4ed832bb118b53f2f01b875c.jpg",
                 height: 100,
-              )
+              ),
+              const Icon(
+                Icons.error,
+                color: Colors.green,
+              ),
+              //TODO:如何加上选中和未选中文字显示
+              Switch(
+                  value: _switchSelected,
+                  activeColor: Colors.green,
+                  onChanged: (value) {
+                    setState(() {
+                      _switchSelected = value;
+                    });
+                  }),
+              Checkbox(
+                  value: _checkboxSelected,
+                  onChanged: (value) {
+                    setState(() {
+                      _checkboxSelected = value!;
+                    });
+                  }),
+              //获取输入内容有两种方式：
+              // 1、定义两个变量，用于保存用户名和密码，然后在onChange触发时，各自保存一下输入内容。
+              // 2、通过controller直接获取。
+              // 参考文档：https://book.flutterchina.club/chapter3/input_and_form.html#_3-5-1-textfield
+              const TextField(
+                decoration: InputDecoration(
+                    labelText: "用户名",
+                    hintText: "用户名或邮箱",
+                    enabledBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.grey)),
+                    focusedBorder: UnderlineInputBorder(
+                        borderSide: BorderSide(color: Colors.blue)),
+                    prefixIcon: Icon(Icons.person)),
+              ),
+              const TextField(
+                decoration: InputDecoration(
+                    labelText: '密码',
+                    hintText: '您的登录密码',
+                    prefixIcon: Icon(Icons.lock)),
+                obscureText: true,
+              ),
+              Form(
+                  key: _formKey,
+                  autovalidateMode: AutovalidateMode.onUserInteraction,
+                  child: Column(
+                    children: [
+                      TextFormField(
+                        controller: _unameController,
+                        decoration: const InputDecoration(
+                            labelText: '用户名',
+                            hintText: '请输入用户名',
+                            icon: Icon(Icons.person)),
+                        validator: (v) {
+                          return v!.trim().isNotEmpty ? null : '用户名不能为空';
+                        },
+                      ),
+                      ElevatedButton(
+                          onPressed: () {
+                            // 通过_formKey.currentState 获取FormState后，
+                            // 调用validate()方法校验用户名密码是否合法，校验
+                            // 通过后再提交数据。
+                            if ((_formKey.currentState as FormState)
+                                .validate()) {
+                              //  验证通过提交数据
+                            }
+                          },
+                          child: Text('登录'))
+                    ],
+                  ))
             ],
           ),
         ),
